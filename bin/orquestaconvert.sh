428a7e7db5ffea6726ea98bc21fd520bb65cceaa
blag <blag@users.noreply.github.com>
2018-09-28 12:55:49 -0700

    Pass the VIRTUALENV_DIR variable as an environment variable to make

--- bin/orquestaconvert.sh
+++ bin/orquestaconvert.sh
@@ -3,7 +3,7 @@ ORQUESTACONVERT_DIR="${SCRIPT_DIR}/.."
 VIRTUALENV_DIR="${ORQUESTACONVERT_DIR}/virtualenv"
 
 # create virtualenv if it doesn't exit
-test -d "$VIRTUALENV_DIR" || make -C $ORQUESTACONVERT_DIR requirements
+test -d "$VIRTUALENV_DIR" || VIRTUALENV_DIR=$VIRTUALENV_DIR make -C $ORQUESTACONVERT_DIR requirements
 
 # activate the virtualenv
 source ${VIRTUALENV_DIR}/bin/activate

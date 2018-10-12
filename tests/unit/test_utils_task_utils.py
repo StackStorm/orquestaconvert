bfd4bcf87311ea5ba4af7f64aacb029f9db62a17
blag <blag@users.noreply.github.com>
2018-10-12 03:53:44 -0600

    Add tests for task name conversion

---
+++ tests/unit/test_utils_task_utils.py
@@ -0,0 +1,11 @@
+from tests.base_test_case import BaseTestCase
+
+from orquestaconvert.utils import task_utils
+
+
+class TestTaskUtils(BaseTestCase):
+    __test__ = True
+
+    def test_convert_dashes_to_underscores(self):
+        self.assertEquals(task_utils.translate_task_name('foo-bar-baz'), 'foo_bar_baz')
+        self.assertEquals(task_utils.translate_task_name('baz_foo'), 'baz_foo')
